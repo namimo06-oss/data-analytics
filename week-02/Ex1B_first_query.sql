cda45b849392f888085d411421dc9cf7398c83ed
natalia miranda <nmiranda@my.yearupunited.org>
2026-04-13 20:45:24 -0700

    Added queries of actor and film table

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 Sakila is a database used to collect information about _______ and ________ for use by ________.
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved [number] records
+SELECT * FROM film; -- Retrieved [number] records
